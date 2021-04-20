131ad88e869833f7cd43311383943cb68c5603e8
Inarus <maciejstop@gmail.com>
2021-04-20 20:03:27 +0200

    Update Badges.php

--- src/Rest/Badges.php
+++ src/Rest/Badges.php
@@ -8,13 +8,12 @@ class Badges extends AbstractRestResource
 {
     public function badgecampaigns()
     {
-    //  echo 'dupa';
-
     $requestHeaders = [
         'Accept' => 'application/vnd.allegro.beta.v1+json',
         'Content-Type' => 'application/vnd.allegro.beta.v1+json',
     ];
         return $this->get('/sale/badge-campaigns', $requestHeaders);
     }
+    
 
 }
