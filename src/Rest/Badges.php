777da80ba176a8872317ebaa733f98c0e19c1844
Inarus <maciejstop@gmail.com>
2021-04-20 21:44:42 +0200

    Update Badges.php

--- src/Rest/Badges.php
+++ src/Rest/Badges.php
@@ -6,6 +6,8 @@ use Ircykk\AllegroApi\Rest\AbstractRestResource;
 
 class Badges extends AbstractRestResource
 {
+    // List of available campaigns
+
     public function badgecampaigns()
     {
     $requestHeaders = [
@@ -14,6 +16,32 @@ class Badges extends AbstractRestResource
     ];
         return $this->get('/sale/badge-campaigns', $requestHeaders);
     }
-    
+
+    //Submit offer to a campaign
+
+    public function badgesubmit($campaign, $offer_id, $market_amount, $bargain_amount)
+    {
+    $requestHeaders = [
+        'Accept' => 'application/vnd.allegro.beta.v1+json',
+        'Content-Type' => 'application/vnd.allegro.beta.v1+json',
+    ];
+    $params = [
+      'campaign' => ['id' => $campaign,],
+      'offer' =>    [ 'id' => $offer_id,],
+      'prices' =>   [
+                    'market' => [ 'amount' => $market_amount,
+                                  'currency' => 'PLN',],
+                    'bargain' =>
+                                [
+                                  'amount' => $bargain_amount,
+                                  'currency' => 'PLN',],
+                    ],
+    ];
+
+
+        return $this->post('/sale/badges', $params, $requestHeaders);
+    }
+
+
 
 }
